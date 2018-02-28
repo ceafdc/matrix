b2934a38d7b9f209ef2bec2433efaba4f2bf6aa5
Gonzo Fialho <carloseaf@gmail.com>
2018-02-28 14:46:10 -0300

    Add MIN and MAX macros

--- matrix.c
+++ matrix.c
@@ -6,6 +6,16 @@
 #include <string.h>
 #include "matrix.h"
 
+#define MAX(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+#define MIN(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
 double **
 init_data(int m, int n) {
     double **data = (double **)malloc(sizeof(double *) * m);
